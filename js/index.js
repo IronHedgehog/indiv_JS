82a9b6b02dd3059e302c6745cec1ee2d8c5d9ba0
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2026-01-09 19:08:19 +0200

    Numbers

--- js/index.js
+++ js/index.js
@@ -12,29 +12,42 @@
 // Порівняння
 // <= , >=, ===, ==, >, < - true або false
 
-console.log("12" == 12); // == - не суворе порівняння (використовувати коли треба кастиль)
-console.log("12" === 12); // === - суворе порівняння(використовувати завжди)
+//console.log("12" == 12); // == - не суворе порівняння (використовувати коли треба кастиль)
+//console.log("12" === 12); // === - суворе порівняння(використовувати завжди)
 
 // isNaN - перевірка на НЕ число
 
-console.log(isNaN(12));
+// console.log(isNaN(12));
 
-if (isNaN(12)) {
-  console.log("Введи число");
-} else {
-  console.log("Все ок, можна");
-}
+// if (isNaN(12)) {
+//   console.log("Введи число");
+// } else {
+//   console.log("Все ок, можна");
+// }
 
 // MATH -
 
-console.log(Math.floor(Math.random() * 100)); // random
+// console.log(Math.floor(Math.random() * 100)); // random
 
 // console.log(Math.max(12, 13, 14, 15, 10));
 // console.log(Math.min(12, 13, 14, 15, 10));
 
-console.log(Math.ceil(Math.random()));
-console.log(Math.floor(Math.random())); // +
-console.log(Math.round(Math.random()));
+// console.log(Math.ceil(Math.random()));
+// console.log(Math.floor(Math.random()));
+// console.log(Math.round(Math.random()));
 
 // console.log(Math.pow(2, 5));
 // console.log(Math.sqrt(100));
+
+console.log((0.1 * 10 + 0.2 * 10) / 10);
+
+const sum = parseFloat(prompt("На скільки бажаєте попвнити баланс?"));
+
+// parseInt
+// parseFloat
+
+console.log(sum + 10);
+
+const sum1 = 100;
+
+console.log(100 * 0.1);
